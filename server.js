908e0880491095400016b5307239cf2bd2428bf9
Kamol774 <chust1222@gmail.com>
2024-02-03 23:01:46 +0500

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const express = require("express");
 const res = require("express/lib/response");
 const app = express();     // expressning 'app' objectini yaratamiz
 const http = require("http");
+const fs = require("fs");
+
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data)
+  }
+});
 
 // 1: Kirish code      express - framework
 app.use(express.static("public"));   // har qanday kelayotgan zaproslar uchun 'public' folderni clientlarga ochib beryapmiz
@@ -12,15 +23,20 @@ app.use(express.urlencoded({extended: true}));   // (traditional) form request d
 
 // 2: Session code  -> xozircha foydalanmaymiz
 
-// 3: Views code
-app.set("views", "views");    
-app.set("view engine", "ejs");   //  BSSR (backend-server-side-rendering) backend da frontend ni (quramiz) ishga tushiramiz
+// 3: Views code    (backend da frontend ni yasaymiz) BSSR (backend-server-side-rendering) backend da frontend ni (quramiz) ishga tushiramiz
+app.set("views", "views");    // html~css ma'lumotlarini 'views' papkasini ichidan o'qiydi deb ko'rsatib o'tyapmiz 
+app.set("view engine", "ejs");   //  'view engine'-miz 'ejs' ekanligini ko'rsatib o'tyapmiz
 
 // 4: Routing code  (har bir page uchun link yaratamiz)
 app.post("/create-item", (req, res) => {
-  console.log(req.body);
-  res.json({test: "success"});
-})
+  //   TODO: code with db here
+  // console.log(req.body);
+  // res.json({test: "success"});
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", {user: user});
+});
 
 app.get("/", function(req, res) {
   res.render("harid");
--- train.js
+++ train.js
@@ -1,37 +1,42 @@
-console.log("Dastlabki vazifa");
-console.log("");
 
-const calculate = require('./hisob.js');
 
-const natija = calculate.kopaytirish(80, 30);
-console.log("Natija:", natija);
-console.log("=====================");
 
-const natija1 = calculate.qoshish(80, 30);
-console.log("Natija:", natija1);
-console.log("=====================");
 
-const natija2 = calculate.ayirish(80, 30);
-console.log("Natija:", natija2);
-console.log("=====================");
 
-const natija3 = calculate.bulish(80, 30);
-console.log("Natija:", natija3);
+// console.log("Dastlabki vazifa");
+// console.log("");
 
-console.log("========================================================================");
+// const calculate = require('./hisob.js');
+
+// const natija = calculate.kopaytirish(80, 30);
+// console.log("Natija:", natija);
+// console.log("=====================");
+
+// const natija1 = calculate.qoshish(80, 30);
+// console.log("Natija:", natija1);
+// console.log("=====================");
+
+// const natija2 = calculate.ayirish(80, 30);
+// console.log("Natija:", natija2);
+// console.log("=====================");
+
+// const natija3 = calculate.bulish(80, 30);
+// console.log("Natija:", natija3);
+
+// console.log("========================================================================");
 
  
 
-const Account = require("./account");
+// const Account = require("./account");
 
-Account.tellMeAboutClass();
-Account.tellMeTime();
-const myAccount = new Account('Kevin', 250000, 4568645642315263486);
+// Account.tellMeAboutClass();
+// Account.tellMeTime();
+// const myAccount = new Account('Kevin', 250000, 4568645642315263486);
 
-myAccount.tellMeBalance();
-console.log("*****************")
-myAccount.withdrowMoney(150000);
-console.log("*****************")
-myAccount.makeDeposit(77777);
-console.log("*****************")
-myAccount.giveMeDetails();
+// myAccount.tellMeBalance();
+// console.log("*****************")
+// myAccount.withdrowMoney(150000);
+// console.log("*****************")
+// myAccount.makeDeposit(77777);
+// console.log("*****************")
+// myAccount.giveMeDetails();
