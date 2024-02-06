7d093e9fe2698bd213ca7e038e45925f2bf6ddd6
Kamol774 <chust1222@gmail.com>
2024-02-06 19:48:39 +0500

    portfolio publishing DONE

--- train.js
+++ train.js
@@ -1,8 +1,8 @@
-// TASK 18-19 [express, ejs]
+// TASK 20 [ejs da portfolio publishing qilish]
 
 const express = require("express");
 const res = require("express/lib/response");
-const app = express();     // expressning 'app' objectini yaratamiz
+const app = express();     
 const http = require("http");
 const fs = require("fs");
 
@@ -16,31 +16,16 @@ fs.readFile("database/user.json", "utf8", (err, data) => {
   }
 });
 
-// 1: Kirish code      
 app.use(express.static("public"));   
 app.use(express.json());     
 app.use(express.urlencoded({extended: true}));  
 
-// 2: Session code  -> xozircha foydalanmaymiz
-
-// 3: Views code    
 app.set("views", "views");   
 app.set("view engine", "ejs");  
-
-// 4: Routing code  (har bir page uchun link yaratamiz)
-app.post("/create-item", (req, res) => {
-  console.log(req.body);
-  res.json({test: "success"});
-});
 
 app.get("/author", (req, res) => {
   res.render("author", {user: user});
 });
-
-app.get("/", function(req, res) {
-  res.render("harid");
-});
-
 
 const server = http.createServer(app);
 let PORT = 3000;
@@ -69,7 +54,80 @@ server.listen(PORT, function() {
 
 
 
+// TASK 18-19 [express, ejs]
+
+// const express = require("express");
+// const res = require("express/lib/response");
+// const app = express();     // expressning 'app' objectini yaratamiz
+// const http = require("http");
+// const fs = require("fs");
+
+
+// let user;
+// fs.readFile("database/user.json", "utf8", (err, data) => {
+//   if (err) {
+//     console.log("ERROR:", err);
+//   } else {
+//     user = JSON.parse(data)
+//   }
+// });
+
+// // 1: Kirish code      
+// app.use(express.static("public"));   
+// app.use(express.json());     
+// app.use(express.urlencoded({extended: true}));  
+
+// // 2: Session code  -> xozircha foydalanmaymiz
+
+// // 3: Views code    
+// app.set("views", "views");   
+// app.set("view engine", "ejs");  
+
+// // 4: Routing code  (har bir page uchun link yaratamiz)
+// app.post("/create-item", (req, res) => {
+//   console.log(req.body);
+//   res.json({test: "success"});
+// });
+
+// app.get("/author", (req, res) => {
+//   res.render("author", {user: user});
+// });
+
+// app.get("/", function(req, res) {
+//   res.render("harid");
+// });
+
+
+// const server = http.createServer(app);
+// let PORT = 3000;
+// server.listen(PORT, function() {
+//   console.log(`The server is running successfully on port: ${PORT}`);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // console.log("Dastlabki vazifa");
+
+
 // console.log("");
 
 // const calculate = require('./hisob.js');
