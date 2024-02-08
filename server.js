752de5d759cdd63aeb1222c5f7f0a98ac686f9bc
Kamol774 <chust1222@gmail.com>
2024-02-08 18:49:39 +0500

    feat: train callback and async functions

--- server.js
+++ server.js
@@ -14,13 +14,15 @@ fs.readFile("database/user.json", "utf8", (err, data) => {
   }
 });
 
-// 1: Kirish code      express - framework
+// 1: Kirish code      express - framework   app.use ->middleware
 app.use(express.static("public"));   // har qanday kelayotgan zaproslar uchun 'public' folderni clientlarga ochib beryapmiz
 app.use(express.json());     // kelayotgan 'json' formatdagi datani bizga object holatiga o'girib beradi 
 app.use(express.urlencoded({extended: true}));   // (traditional) form request dan POST qilingan narsalarni express server qabul qilishi uchun
 
 // 2: Session code  -> xozircha foydalanmaymiz
 
+
+// app.set -> configuration
 // 3: Views code    (backend da frontend ni yasaymiz) BSSR (backend-server-side-rendering) backend da frontend ni (quramiz) ishga tushiramiz
 app.set("views", "views");    // html~css ma'lumotlarini 'views' papkasini ichidan o'qiydi deb ko'rsatib o'tyapmiz 
 app.set("view engine", "ejs");   //  'view engine'-miz 'ejs' ekanligini ko'rsatib o'tyapmiz
--- train.js
+++ train.js
@@ -1,166 +1,73 @@
-// TASK 20 [ejs da portfolio publishing qilish]
+// A-TASK
 
-const express = require("express");
-const res = require("express/lib/response");
-const app = express();     
-const http = require("http");
-const fs = require("fs");
+function countletter(a, b) {
+  list = b.split('').filter((ele, index) => {
+    return ele === a;  
+  });
+  total = list.length;
+  console.log(`Berilgan "${b}" so'zida ${total}ta "${a}" harfi mavjud`);
+}
 
+countletter('a', 'assalomalekum');
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data)
-  }
-});
+// Callback va Asynchronous functionlarni qo'llash:
 
-app.use(express.static("public"));   
-app.use(express.json());     
-app.use(express.urlencoded({extended: true}));  
+// console.log("Jack Ma maslahatlalri");
+// console.log("*********************");
 
-app.set("views", "views");   
-app.set("view engine", "ejs");  
-
-app.get("/author", (req, res) => {
-  res.render("author", {user: user});
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function() {
-  console.log(`The server is running successfully on port: ${PORT}`);
-});
+// const list = [
+//   "yaxshi talaba bo'ling",    // 0-20 yoshlar oralig'i
+//   "to'g'ri boshliq tanlang va ko'proq xato qiling",    // 20-30 yoshlar oralig'i
+//   "o'zingizga ishlashni boshlang",     // 30-40 yoshlar oralig'i  
+//   "siz kuchli bo'lgan narsalarni qiling",     // 40-50 yoshlar oralig'i
+//   "yoshlarga investitsiya qiling",    // 50-60 yoshlar oralig'i
+//   "endi dam oling, foydasi yo'q"      // 60 yoshdan yuqori
+// ];
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// TASK 18-19 [express, ejs]
-
-// const express = require("express");
-// const res = require("express/lib/response");
-// const app = express();     // expressning 'app' objectini yaratamiz
-// const http = require("http");
-// const fs = require("fs");
-
-
-// let user;
-// fs.readFile("database/user.json", "utf8", (err, data) => {
-//   if (err) {
-//     console.log("ERROR:", err);
-//   } else {
-//     user = JSON.parse(data)
+//    // CALLBACK function
+// function maslahatBering(a, callback) {
+//   if(typeof a !== 'number') callback("Please, insert a number", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a > 20 && a <= 30) callback(null, list[1]);
+//   else if (a > 30 && a <= 40) callback(null, list[2]);
+//   else if (a > 40 && a <= 50) callback(null, list[3]);
+//   else if (a > 50 && a <= 60) callback(null, list[4]);
+//   else {
+//     setTimeout(function() {
+//       callback(null, list[5]);
+//     }, 3000);
 //   }
+// }
+
+// console.log('passed here: 0');
+// maslahatBering(65, (err, data) => {
+//   if (err) console.log('ERRoR:', err);
+//   else console.log('javob:', data);
+// });
+// console.log('passed here: 1');
+
+
+
+      
+
+
+
+//       // call via -> async & await
+// async function run() {
+//   console.log('passed here: 0');
+//   javob = await maslahatBering(70);
+//   console.log(javob)
+//   console.log('passed here: 1');
+//   }
+//   run()
+
+//       // call via -> then & catch
+// maslahatBering(65)
+// .then(data => {
+//   console.log('javob:', data);
+// })
+// .catch(err => {
+//   console.log('ERoR',err)
 // });
 
-// // 1: Kirish code      
-// app.use(express.static("public"));   
-// app.use(express.json());     
-// app.use(express.urlencoded({extended: true}));  
-
-// // 2: Session code  -> xozircha foydalanmaymiz
-
-// // 3: Views code    
-// app.set("views", "views");   
-// app.set("view engine", "ejs");  
-
-// // 4: Routing code  (har bir page uchun link yaratamiz)
-// app.post("/create-item", (req, res) => {
-//   console.log(req.body);
-//   res.json({test: "success"});
-// });
-
-// app.get("/author", (req, res) => {
-//   res.render("author", {user: user});
-// });
-
-// app.get("/", function(req, res) {
-//   res.render("harid");
-// });
-
-
-// const server = http.createServer(app);
-// let PORT = 3000;
-// server.listen(PORT, function() {
-//   console.log(`The server is running successfully on port: ${PORT}`);
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log("Dastlabki vazifa");
-
-
-// console.log("");
-
-// const calculate = require('./hisob.js');
-
-// const natija = calculate.kopaytirish(80, 30);
-// console.log("Natija:", natija);
-// console.log("=====================");
-
-// const natija1 = calculate.qoshish(80, 30);
-// console.log("Natija:", natija1);
-// console.log("=====================");
-
-// const natija2 = calculate.ayirish(80, 30);
-// console.log("Natija:", natija2);
-// console.log("=====================");
-
-// const natija3 = calculate.bulish(80, 30);
-// console.log("Natija:", natija3);
-
-// console.log("========================================================================");
-
- 
-
-// const Account = require("./account");
-
-// Account.tellMeAboutClass();
-// Account.tellMeTime();
-// const myAccount = new Account('Kevin', 250000, 4568645642315263486);
-
-// myAccount.tellMeBalance();
-// console.log("*****************")
-// myAccount.withdrowMoney(150000);
-// console.log("*****************")
-// myAccount.makeDeposit(77777);
-// console.log("*****************")
-// myAccount.giveMeDetails();
