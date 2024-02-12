4d97c0dc74755ff7df82799dc62d30e943305e9e
Kamol774 <chust1222@gmail.com>
2024-02-12 10:37:54 +0500

    feat: CRUD operations

--- app.js
+++ app.js
@@ -31,9 +31,18 @@ app.set("view engine", "ejs");   //  'view engine'-miz 'ejs' ekanligini ko'rsati
 
 // 4: Routing code  (har bir page uchun link yaratamiz)
 app.post("/create-item", (req, res) => {
-  //   TODO: code with db here
-  // console.log(req.body);
-  // res.json({test: "success"});
+  console.log('user entered /create-item');
+  //   TODO: code with db here  
+  console.log(req.body);
+  const new_reja = req.body.reja
+  db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end("something went wrong");
+    } else {
+      res.end("successfully added");
+    }
+  });
 });
 
 app.get("/author", (req, res) => {
@@ -41,7 +50,15 @@ app.get("/author", (req, res) => {
 });
 
 app.get("/", function(req, res) {
-  res.render("reja");
+  console.log('user entered /');
+  db.collection("plans").find().toArray((err, data) => {
+    if (err) {
+      console.log(err);
+      res.end("something went wrong");
+    } else {
+      res.render("reja", {items: data});
+    }
+  })
 });
 
 
